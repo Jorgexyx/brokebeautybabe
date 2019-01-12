766eb03d2db390ae32d1f22f3aeb2939c085908b
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2019-01-12 10:34:40 -0800

    added app.use

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const path = require('path');
 //const sephora = require('../sephoraAPI/index.js');
 const sephora = require('./sephoraScrapper.js');
 
 //create a get route
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/express_backend', (req,res) => {
 		sephora.scrapSephoraItems().then(items => {
 			res.send({express: items});
