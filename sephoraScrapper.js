f59a6309dbccbc4dc3584bdc77a616148cdfda0a
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2019-01-12 14:36:47 -0800

    updated sephora scrapper for heroku

--- sephoraScrapper.js
+++ sephoraScrapper.js
@@ -32,8 +32,8 @@ async function pageScroll(page){
 let scrapSephoraItems = async() => {
 	 //Load sephora sales page
 	const browser = await puppeteer.launch({
-		headless: false,
-    //args: ['--no-sandbox', '--disable-setuid-sandbox']
+		//headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
 	});
 	const webPage = await browser.newPage();
 
