decb7f9075ea6159adbf060113426e39a4a11b0b
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2019-01-16 19:49:34 -0800

    update

--- database/getDataFromDb.js
+++ database/getDataFromDb.js
@@ -1,14 +1,29 @@
 const connectToDb = require('./connectToDb.js');
 const firestore = connectToDb.firestore;
 
-var collections = ['Makeup', 'Skincare', 'bathAndBody', 'fragrence', 'hair', 'men', 'miniSize', 'toolsAndBrushes'];
-for (collectionName of collections) {
-	var collection = firestore.collection(collectionName);
-	var query = collection.get().then( snapShot => {
+let getItemsFromDb = async() => {
+	var collections = ['Makeup', 'Skincare', 'bathAndBody', 'fragrence', 'hair', 'men', 'miniSize', 'toolsAndBrushes'];
+	var items = [];
+	for (collectionName of collections) {
+		var collection = firestore.collection(collectionName);
+		var query = await collection.get().then( snapShot => {
 			snapShot.forEach( doc => {
-				console.log(doc.id, '=>', doc.data());
+				items.push(doc.data());
+				//console.log(doc.id, '=>', doc.data());
 			});
-	}).catch(err => {
-		console.log("There was an error:", err);
-	});
+		}).catch(err => {
+			console.log("There was an error:", err);
+		});
+	}
+	return items;
 }
+
+
+/*
+getItemsFromDb().then( (items) => {
+	console.log('done');
+	console.log(items);
+});
+*/
+
+module.exports = { getItemsFromDb };
