8aec1b52c5bdd622896781ea4d80d223507bd656
Jorge <jorge@Nathans-MacBook-Pro-3.local>
2019-01-16 19:59:40 -0800

    updated with database support

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const path = require('path');
-//const sephora = require('../sephoraAPI/index.js');
-const sephora = require('./sephoraScrapper.js');
+//const sephora = require('./sephoraScrapper.js');
+const dbItems = require('./database/getDataFromDb.js');
+
 
 //create a get route
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/express_backend', (req,res) => {
-		sephora.scrapSephoraItems().then(items => {
+		//sephora.scrapSephoraItems().then(items => {
+		dbItems.getItemsFromDb().then( items => {
 			res.send({express: items});
 		}).catch(err => {
 			console.log("there was an error: ", err);
